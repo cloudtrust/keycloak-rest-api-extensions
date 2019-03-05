7b94d28a8c38a727012d569cba25407ed48dbaa0
Christophe Frattino <christophe.frattino@elca.ch>
2019-03-05 16:24:36 +0100

    Override equals() and hashcode()

--- src/main/java/io/cloudtrust/keycloak/representations/idm/CredentialRepresentation.java
+++ src/main/java/io/cloudtrust/keycloak/representations/idm/CredentialRepresentation.java
@@ -1,5 +1,7 @@
 package io.cloudtrust.keycloak.representations.idm;
 
+import java.util.Objects;
+
 public class CredentialRepresentation extends org.keycloak.representations.idm.CredentialRepresentation {
 
     private String id;
@@ -12,5 +14,18 @@ public class CredentialRepresentation extends org.keycloak.representations.idm.C
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        CredentialRepresentation that = (CredentialRepresentation) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id);
+    }
 
 }
