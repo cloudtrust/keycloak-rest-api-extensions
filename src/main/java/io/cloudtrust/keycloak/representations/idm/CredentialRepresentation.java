1d913ca59befb30252659a64c04b196ae86d3d43
Christophe Frattino <christophe.frattino@elca.ch>
2019-03-05 16:11:11 +0100

    Expose the credential id as well

---
+++ src/main/java/io/cloudtrust/keycloak/representations/idm/CredentialRepresentation.java
@@ -0,0 +1,16 @@
+package io.cloudtrust.keycloak.representations.idm;
+
+public class CredentialRepresentation extends org.keycloak.representations.idm.CredentialRepresentation {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+}
