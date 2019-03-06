f60186b4627993267831a3455eb374eadd320b68
Christophe Frattino <christophe.frattino@elca.ch>
2019-03-06 16:27:45 +0100

    Return a specific credential from its id

--- src/main/java/io/cloudtrust/keycloak/services/resource/api/CredentialsResource.java
+++ src/main/java/io/cloudtrust/keycloak/services/resource/api/CredentialsResource.java
@@ -1,17 +1,18 @@
 package io.cloudtrust.keycloak.services.resource.api;
 
 import org.jboss.resteasy.annotations.cache.NoCache;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.credential.CredentialModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 import io.cloudtrust.keycloak.representations.idm.CredentialRepresentation ;
+import org.keycloak.services.ForbiddenException;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
 import org.keycloak.services.resources.admin.permissions.UserPermissionEvaluator;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,6 +42,20 @@ public class CredentialsResource {
 
         return session.userCredentialManager().getStoredCredentials(realm, user).stream().map(this::toRepresentation)
                 .collect(Collectors.toList());
+    }
+
+    @Path("{id}")
+    @GET
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    public CredentialRepresentation credentials(final @PathParam("id") String id) {
+        CredentialModel credential = session.userCredentialManager().getStoredCredentialById(realm, user, id);
+        if (credential == null) {
+            // we do this to make sure somebody can't phish ids
+            if (auth.users().canQuery()) throw new NotFoundException("Credential not found");
+            else throw new ForbiddenException();
+        }
+        return toRepresentation(session.userCredentialManager().getStoredCredentialById(realm, user, id));
     }
 
     public CredentialRepresentation toRepresentation(CredentialModel model) {
