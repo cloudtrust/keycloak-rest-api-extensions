b7e2ed930f6eec1f30bd3e656bf2691301a047ad
Christophe Frattino <christophe.frattino@elca.ch>
2019-03-13 10:18:42 +0100

    Delete a credential

--- src/main/java/io/cloudtrust/keycloak/services/resource/api/CredentialsResource.java
+++ src/main/java/io/cloudtrust/keycloak/services/resource/api/CredentialsResource.java
@@ -1,16 +1,20 @@
 package io.cloudtrust.keycloak.services.resource.api;
 
+import io.cloudtrust.keycloak.representations.idm.CredentialRepresentation;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
-import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.keycloak.credential.CredentialModel;
+import org.keycloak.credential.UserCredentialStore;
+import org.keycloak.events.admin.OperationType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
-import io.cloudtrust.keycloak.representations.idm.CredentialRepresentation ;
+import org.keycloak.services.ErrorResponse;
 import org.keycloak.services.ForbiddenException;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
 import org.keycloak.services.resources.admin.permissions.UserPermissionEvaluator;
+import org.keycloak.storage.StorageId;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -18,6 +22,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CredentialsResource {
+
+    private static final Logger logger = Logger.getLogger(CredentialsResource.class);
 
     private final RealmModel realm;
     private final UserModel user;
@@ -48,7 +54,7 @@ public class CredentialsResource {
     @GET
     @NoCache
     @Produces(MediaType.APPLICATION_JSON)
-    public CredentialRepresentation credentials(final @PathParam("id") String id) {
+    public CredentialRepresentation getCredential(final @PathParam("id") String id) {
         CredentialModel credential = session.userCredentialManager().getStoredCredentialById(realm, user, id);
         if (credential == null) {
             // we do this to make sure somebody can't phish ids
@@ -56,6 +62,31 @@ public class CredentialsResource {
             else throw new ForbiddenException();
         }
         return toRepresentation(session.userCredentialManager().getStoredCredentialById(realm, user, id));
+    }
+
+    @Path("{id}")
+    @DELETE
+    public void deleteCredential(final @PathParam("id") String id) {
+        auth.users().requireManage(user);
+        try {
+            UserCredentialStore ucs;
+            if (StorageId.isLocalStorage(user)) {
+                ucs = (UserCredentialStore) session.userLocalStorage();
+            } else {
+                ucs = (UserCredentialStore) session.userFederatedStorage();
+            }
+            if (ucs.removeStoredCredential(realm, user, id)) {
+                adminEvent.operation(OperationType.UPDATE).resourcePath(session.getContext().getUri()).success();
+                if (session.getTransactionManager().isActive()) {
+                    session.getTransactionManager().commit();
+                }
+            } else {
+                logger.warn("Could not delete credential " + id + " of user " + user.getUsername() +
+                        "! removeStoredCredential() returned false.");
+            }
+        } catch (Exception me) {
+            logger.warn("Could not delete credential " + id + " of user " + user.getUsername() + "!", me);
+        }
     }
 
     public CredentialRepresentation toRepresentation(CredentialModel model) {
