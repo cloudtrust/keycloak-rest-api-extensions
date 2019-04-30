80870e636999ae2a58d7fe3c1de5ef238903a928
harture <harture414@gmail.com>
2019-04-30 17:21:38 +0200

    Extend User creation to manage roles and groups

--- src/main/java/io/cloudtrust/keycloak/services/resource/api/admin/UsersResource.java
+++ src/main/java/io/cloudtrust/keycloak/services/resource/api/admin/UsersResource.java
@@ -1,31 +1,30 @@
 package io.cloudtrust.keycloak.services.resource.api.admin;
 
 import io.cloudtrust.keycloak.services.NotImplementedException;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.ResourceType;
-import org.keycloak.models.GroupModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.models.*;
+import org.keycloak.models.utils.ModelToRepresentation;
+import org.keycloak.models.utils.RepresentationToModel;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.services.ErrorResponse;
 import org.keycloak.services.ForbiddenException;
 import org.keycloak.services.resources.admin.AdminEventBuilder;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import javax.ws.rs.core.Response;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UsersResource extends org.keycloak.services.resources.admin.UsersResource {
+
+    private static final Logger logger = Logger.getLogger(UsersResource.class);
 
     private AdminPermissionEvaluator auth;
     private AdminEventBuilder adminEvent;
@@ -36,6 +35,93 @@ public class UsersResource extends org.keycloak.services.resources.admin.UsersRe
         this.auth = auth;
         this.session = session;
         this.adminEvent = adminEvent.resource(ResourceType.USER);
+    }
+
+    /**
+     * Create a new user.
+     * This extended API allows to assign groups and roles at user creation.
+     *
+     * Username must be unique.
+     *
+     * @param rep
+     * @return
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(final UserRepresentation rep) {
+        // Security checks
+        auth.users().requireManage();
+
+        List<GroupModel> groups = new ArrayList<>();
+        List<RoleModel> roles = new ArrayList<>();
+
+        for (String groupId: rep.getGroups()) {
+            GroupModel group = session.realms().getGroupById(groupId, realm);
+
+            if (group == null) {
+                throw new org.jboss.resteasy.spi.NotFoundException("Group not found");
+            }
+
+            auth.groups().requireManageMembership(group);
+            groups.add(group);
+        }
+
+        for (String roleId: rep.getRealmRoles()){
+            RoleModel role = realm.getRoleById(roleId);
+
+            if (role == null ) {
+                throw new org.jboss.resteasy.spi.NotFoundException("Role not found");
+            }
+
+            auth.roles().requireMapRole(role);
+            roles.add(role);
+        }
+
+
+        // Double-check duplicated username and email here due to federation
+        if (session.users().getUserByUsername(rep.getUsername(), realm) != null) {
+            return ErrorResponse.exists("User exists with same username");
+        }
+        if (rep.getEmail() != null && !realm.isDuplicateEmailsAllowed() && session.users().getUserByEmail(rep.getEmail(), realm) != null) {
+            return ErrorResponse.exists("User exists with same email");
+        }
+
+        try {
+            UserModel user = session.users().addUser(realm, rep.getUsername());
+            Set<String> emptySet = Collections.emptySet();
+
+            org.keycloak.services.resources.admin.UserResource.updateUserFromRep(user, rep, emptySet, realm, session, false);
+            RepresentationToModel.createCredentials(rep, session, realm, user, true);
+
+            // Add groups
+            for (GroupModel group: groups) {
+                user.joinGroup(group);
+            }
+
+            // Add roles
+            for (RoleModel role: roles) {
+                user.grantRole(role);
+            }
+
+            adminEvent.operation(OperationType.CREATE).resourcePath(session.getContext().getUri(), user.getId()).representation(rep).success();
+
+            if (session.getTransactionManager().isActive()) {
+                session.getTransactionManager().commit();
+            }
+
+            return Response.created(session.getContext().getUri().getAbsolutePathBuilder().path(user.getId()).build()).build();
+        } catch (ModelDuplicateException e) {
+            if (session.getTransactionManager().isActive()) {
+                session.getTransactionManager().setRollbackOnly();
+            }
+            return ErrorResponse.exists("User exists with same username or email");
+        } catch (ModelException me){
+            if (session.getTransactionManager().isActive()) {
+                session.getTransactionManager().setRollbackOnly();
+            }
+            logger.warn("Could not create user", me);
+            return ErrorResponse.exists("Could not create user");
+        }
     }
 
     /**
