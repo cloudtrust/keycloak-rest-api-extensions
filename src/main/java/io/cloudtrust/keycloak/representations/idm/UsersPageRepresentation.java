351516397aeaf29106b940df0609d0d2a471db41
Christophe Frattino <christophe.frattino@elca.ch>
2019-06-28 14:37:55 +0200

    CLOUDTRUST-1425 Adapt existing unit tests. Remove deprecated unit tests. Add new unit test.

--- src/main/java/io/cloudtrust/keycloak/representations/idm/UsersPageRepresentation.java
+++ src/main/java/io/cloudtrust/keycloak/representations/idm/UsersPageRepresentation.java
@@ -6,8 +6,21 @@ import java.util.List;
 
 public class UsersPageRepresentation {
 
+    /**
+     * Users
+     */
     List<UserRepresentation> users;
+
+    /**
+     * Total users count
+     */
     int count;
+
+    /**
+     * For unserializing
+     */
+    protected UsersPageRepresentation() {
+    }
 
     public UsersPageRepresentation(List<UserRepresentation> users, int count) {
         this.users = users;
@@ -18,7 +31,15 @@ public class UsersPageRepresentation {
         return users;
     }
 
+    public void setUsers(List<UserRepresentation> users) {
+        this.users = users;
+    }
+
     public int getCount() {
         return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
