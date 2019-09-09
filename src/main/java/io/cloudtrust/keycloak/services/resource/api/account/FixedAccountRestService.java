71319b4e5c83d940abd06a5e5a787085431a0386
rpo <harture414@gmail.com>
2019-09-09 11:38:35 +0200

    Add delete account endpoint

--- src/main/java/io/cloudtrust/keycloak/services/resource/api/account/FixedAccountRestService.java
+++ src/main/java/io/cloudtrust/keycloak/services/resource/api/account/FixedAccountRestService.java
@@ -1,20 +1,27 @@
 package io.cloudtrust.keycloak.services.resource.api.account;
 
+import org.jboss.resteasy.annotations.cache.NoCache;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpResponse;
 import org.keycloak.common.Profile;
+import org.keycloak.events.Details;
 import org.keycloak.events.EventBuilder;
-import org.keycloak.models.ClientModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.UserModel;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.models.*;
+import org.keycloak.services.ErrorResponse;
 import org.keycloak.services.managers.Auth;
+import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.services.resources.Cors;
 import org.keycloak.services.resources.account.AccountCredentialResource;
 import org.keycloak.services.resources.account.AccountRestService;
+import org.keycloak.services.resources.admin.AdminEventBuilder;
 
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
  * This is a copy of org.keycloak.services.resources.account.AccountRestService that fixes a bug with CORS
@@ -25,7 +32,13 @@ public class FixedAccountRestService extends AccountRestService {
     private KeycloakSession session;
     private Auth auth;
     private EventBuilder event;
+
     private UserModel user;
+    private RealmModel realm;
+    private EventBuilder adminEvent;
+
+    @Context
+    private HttpRequest request;
 
     public FixedAccountRestService(KeycloakSession session, Auth auth, ClientModel client, EventBuilder event) {
         super(session, auth, client, event);
@@ -33,6 +46,7 @@ public class FixedAccountRestService extends AccountRestService {
         this.auth = auth;
         this.event = event;
         this.user = auth.getUser();
+        this.realm = auth.getRealm();
     }
 
     @Path("/credentials")
@@ -43,4 +57,37 @@ public class FixedAccountRestService extends AccountRestService {
         }
         return new AccountCredentialResource(session, event, user, auth);
     }
+
+    /**
+     * Delete account
+     *
+     * @return
+     */
+    @Path("/")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @NoCache
+    public Response deleteAccount() {
+        auth.require(AccountRoles.MANAGE_ACCOUNT);
+
+        UserModel user = auth.getUser();
+
+        boolean removed = new UserManager(session).removeUser(realm, user);
+        if (removed) {
+            event.event(EventType.UPDATE_PROFILE).user(user)
+                    .client(auth.getClient())
+                    .detail("ct_event_type", "SELF_DELETE_ACCOUNT")
+                    .detail("username", user.getUsername())
+                    .success();
+            return Cors.add(request, Response.ok()).auth().allowedOrigins(auth.getToken()).build();
+        } else {
+            event.event(EventType.UPDATE_PROFILE).user(user)
+                    .client(auth.getClient())
+                    .detail("ct_event_type", "SELF_DELETE_ACCOUNT_ERROR")
+                    .detail("username", user.getUsername())
+                    .success();
+           return ErrorResponse.error("User couldn't be deleted", Response.Status.BAD_REQUEST);
+        }
+    }
+
 }
