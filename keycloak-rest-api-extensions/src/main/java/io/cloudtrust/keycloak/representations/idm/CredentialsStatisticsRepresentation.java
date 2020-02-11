1cdf5e4631439439e5ff39a3bd636a58afea8c1e
Francis PEROT <francis.perot@elca.ch>
2020-02-11 21:41:25 +0100

    [CLOUDTRUST-2251] Statistics performances enhancement

--- keycloak-rest-api-extensions/src/main/java/io/cloudtrust/keycloak/representations/idm/CredentialsStatisticsRepresentation.java
+++ keycloak-rest-api-extensions/src/main/java/io/cloudtrust/keycloak/representations/idm/CredentialsStatisticsRepresentation.java
@@ -2,6 +2,6 @@ package io.cloudtrust.keycloak.representations.idm;
 
 import java.util.HashMap;
 
-public class CredentialsStatisticsRepresentation extends HashMap<String, Integer> {
-
+public class CredentialsStatisticsRepresentation extends HashMap<String, Long> {
+    private static final long serialVersionUID = 3153265674819318399L;
 }
