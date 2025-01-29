5e5d4a46c4bb888f50de3569e1ebe3f3743f1c78
Kilian Schneiter <33868847+Xelowak@users.noreply.github.com>
2025-01-29 10:03:37 +0100

    [CLOUDTRUST-5806] Trigger an event when an email is sent (#81)

--- keycloak-rest-api-extensions/src/main/java/io/cloudtrust/keycloak/email/EmailSender.java
+++ keycloak-rest-api-extensions/src/main/java/io/cloudtrust/keycloak/email/EmailSender.java
@@ -1,9 +1,12 @@
 package io.cloudtrust.keycloak.email;
 
+import io.cloudtrust.keycloak.Events;
 import io.cloudtrust.keycloak.email.model.EmailModel;
 import io.cloudtrust.keycloak.email.provider.FreeMarkerEmailAttachmentsSenderProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.keycloak.email.EmailException;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.events.EventType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.services.ErrorResponse;
@@ -28,13 +31,26 @@ public class EmailSender {
             }
         }
 
+        String receiverAddress = emailModel.getRecipient();
+        EventBuilder eventBuilder = new EventBuilder(realm, session, session.getContext().getConnection());
+        eventBuilder.event(EventType.CUSTOM_REQUIRED_ACTION)
+                .user(session.users().getUserByEmail(realm, receiverAddress))
+                .detail("receiverAddress", receiverAddress)
+                .detail("themeRealmName", emailModel.getTheming().getThemeRealmName())
+                .detail("locale", emailModel.getTheming().getLocale())
+                .detail("subjectKey", emailModel.getTheming().getSubjectKey())
+                .detail("template", emailModel.getTheming().getTemplate());
+
         try {
             new FreeMarkerEmailAttachmentsSenderProvider(session)
                     .setSmtpConfig(realm.getSmtpConfig())
                     .setDefaultLocale(locale)
                     .send(emailModel, attributes);
+
+            eventBuilder.detail(Events.CT_EVENT_TYPE, "EMAIL_SENT").success();
             return Response.noContent().build();
         } catch (EmailException e) {
+            eventBuilder.detail(Events.CT_EVENT_TYPE, "EMAIL_SENDING_FAILURE").error("Failed to send email");
             throw new WebApplicationException("Can't send mail to " + emailModel.getRecipient(), e);
         }
     }
